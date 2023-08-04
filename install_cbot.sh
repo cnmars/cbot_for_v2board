cbbc9ba3fda8b4a597021d632b264b9c8ae3ac9e
caoyuzhang <qq635677784@gmail.com>
2023-08-04 16:36:21 +0800

    new

--- install_cbot.sh
+++ install_cbot.sh
@@ -40,12 +40,61 @@ install_base() {
     if [[ x"${release}" == x"centos" ]]; then
         yum update -y
         yum install epel-release -y
-        yum install wget curl tar crontabs git -y
+        yum install wget curl tar crontabs sort gcc zlib-devel libffi-devel openssl-devel bzip2-devel  -y
     else
         apt update -y
         apt install wget curl tar cron git -y
     fi
 }
+
+# 安装Python
+install_python() {
+    echo -e "检测python版本..."
+    python_version=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:3])))' 2>/dev/null)
+    if [[ -z $python_version ]]; then
+        python_version="0.0.0"
+    fi
+    echo -e "当前python版本: $python_version"
+    required_version="3.8.0"
+    download_version="3.10.10"
+    extracted_version="${download_version%.*}"
+
+    # 将版本号转换为数字，并进行比较
+    if [[ "$(printf '%s\n' "$python_version" "$required_version" | sort -V | head -n1)" == "$required_version" ]]; then
+        echo "Python版本满足要求..."
+    else
+        echo -e "python版本不满足要求,开始安装python..."
+        if [[ x"${release}" == x"centos" ]]; then
+            # 下载并安装Python
+            temp_dir=$(mktemp -d)
+            cd "$temp_dir"
+            wget https://www.python.org/ftp/python/$download_version/Python-$download_version.tgz
+            tar -xf Python-$download_version.tgz
+            make clean
+            cd Python-$download_version
+            ./configure --enable-optimizations
+            make -j$(nproc)
+            make altinstall
+
+            # 清理临时文件
+            cd ~
+            rm -rf "$temp_dir"
+
+            # 创建新的pip3和python3软链接
+            unlink /usr/bin/pip3 > /dev/null 2>&1
+            ln -s /usr/local/bin/pip$extracted_version /usr/bin/pip3
+            unlink /usr/bin/python3 > /dev/null 2>&1
+            ln -s /usr/local/bin/python$extracted_version /usr/bin/python3
+            
+            python3 -V
+            pip3 -V
+            echo -e "python${extracted_version} 安装成功"
+        else
+            sudo apt update && sudo apt install python3 python3-pip -y
+        fi
+    fi
+}
+
 
 install_cbot(){
     pip3 install python-telegram-bot --upgrade
@@ -81,5 +130,6 @@ if [[ $? != 0 ]]; then
 fi
 
 install_base
+install_python
 install_cbot
 
